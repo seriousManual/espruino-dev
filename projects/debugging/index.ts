b3186c36535102879309d1e124cde848050c593e
Manuel Ernst <manuel.ernst@methodpark.de>
2021-11-25 15:12:48 +0100

    debugging

--- projects/debugging/index.ts
+++ projects/debugging/index.ts
@@ -1,3 +1,10 @@
-setTimeout(() => {
-  digitalPulse(LED1, true, [300, 300, 300, 300, 300, 300, 300])
-}, 500)
+// setTimeout(() => {
+//   digitalPulse(LED1, true, [300, 300, 300, 300, 300, 300, 300])
+// }, 500)
+
+var on = false
+setInterval(function () {
+  on = !on
+  console.log(on)
+  digitalWrite(LED1, on)
+}, 1000)
